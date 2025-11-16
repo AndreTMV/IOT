6f66bf36a61a0434e55fde6245fb9d0f04688a7e
andremejia <a19300104@ceti.mx>
2025-11-15 20:23:20 -0600

    Added admin models

--- sensores/admin.py
+++ sensores/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Sensor, Reading
 
-# Register your models here.
+
+class ReadingInline(admin.TabularInline):
+    model = Reading
+    extra = 0
+    readonly_fields = ("value", "readed_at")
+    ordering = ("-readed_at",)
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "tipo")
+    list_filter = ("tipo",)
+    search_fields = ("name",)
+    inlines = [ReadingInline]
+
+
+@admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+    list_display = ("id", "sensor", "value", "readed_at")
+    list_filter = ("sensor", "readed_at")
+    search_fields = ("sensor__name",)
+    ordering = ("-readed_at",)
+    readonly_fields = ("readed_at",)
